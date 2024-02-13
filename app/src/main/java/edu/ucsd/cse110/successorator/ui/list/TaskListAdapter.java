b0d0f5ede4c61e4e9d6d6835e9d5280c72b8136b
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2024-02-12 22:40:17 -0800

    Its late idk anymore

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/list/TaskListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/list/TaskListAdapter.java
@@ -1,4 +1,80 @@
 package edu.ucsd.cse110.successorator.ui.list;
 
-public class TaskListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import edu.ucsd.cse110.successorator.databinding.ListItemBinding;
+import edu.ucsd.cse110.successorator.lib.domain.Task;
+
+public class TaskListAdapter extends ArrayAdapter<Task> {
+    Consumer<Integer> onDeleteClick;
+    public TaskListAdapter(Context context,
+                           List<Task> tasks,
+                           Consumer<Integer> onDeleteClick) {
+        // This sets a bunch of stuff internally, which we can access
+        // with getContext() and getItem() for example.
+        //
+        // Also note that ArrayAdapter NEEDS a mutable List (ArrayList),
+        // or it will crash!
+        super(context, 0, new ArrayList<>(tasks));
+        this.onDeleteClick=onDeleteClick;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the flashcard for this position.
+        var task = getItem(position);
+        assert task != null;
+
+        // Check if a view is being reused...
+        ListItemBinding binding;
+        if (convertView != null) {
+            // if so, bind to it
+            binding = ListItemBinding.bind(convertView);
+        } else {
+            // otherwise inflate a new view from our layout XML.
+            var layoutInflater = LayoutInflater.from(getContext());
+            binding = ListItemBinding.inflate(layoutInflater, parent, false);
+        }
+
+        // Populate the view with the flashcard's data.
+        // binding..setText(flashcard.front());
+        binding.checkBox.setOnClickListener(v-> {
+            var id=task.getId();
+            assert id !=null;
+            onDeleteClick.accept(id);
+        });
+
+        return binding.getRoot();
+    }
+
+    // The below methods aren't strictly necessary, usually.
+    // But get in the habit of defining them because they never hurt
+    // (as long as you have IDs for each item) and sometimes you need them.
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        var task = getItem(position);
+        assert task != null;
+
+        var id = task.getId();
+        assert id != null;
+
+        return id;
+    }
 }
--- app/src/main/java/edu/ucsd/cse110/successorator/ui/list/TaskListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/list/TaskListFragment.java
@@ -1,4 +1,76 @@
 package edu.ucsd.cse110.successorator.ui.list;
 
-public class TaskListFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsd.cse110.successorator.MainViewModel;
+import edu.ucsd.cse110.successorator.databinding.FragmentCreateMitBinding;
+import edu.ucsd.cse110.successorator.ui.list.dialog.ConfirmDeleteTaskDialogFragment;
+import edu.ucsd.cse110.successorator.ui.list.dialog.CreateTaskDialogFragment;
+
+public class TaskListFragment extends Fragment {
+    private MainViewModel activityModel;
+    private FragmentCardListBinding view;
+    private CardListAdapter adapter;
+
+    public CardListFragment() {
+        // Required empty public constructor
+    }
+
+    public static CardListFragment newInstance() {
+        CardListFragment fragment = new CardListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the Model
+        var modelOwner = requireActivity();
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
+
+        // Initialize the Adapter (with an empty list for now)
+        this.adapter = new CardListAdapter(requireContext(), List.of(), id -> {
+            var dialogFragment = ConfirmDeleteCardDialogFragment.newInstance(id);
+            dialogFragment.show(getParentFragmentManager(), "ConfirmDeleteCardDialogFragment");
+        });
+        activityModel.getOrderedCards().observe(cards -> {
+            if (cards == null) return;
+            adapter.clear();
+            adapter.addAll(new ArrayList<>(cards)); // remember the mutable copy here!
+            adapter.notifyDataSetChanged();
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.view = FragmentCardListBinding.inflate(inflater, container, false);
+
+        // Set the adapter on the ListView
+        view.cardList.setAdapter(adapter);
+
+        view.createCardButton.setOnClickListener(v-> {
+            var dialogFragment= CreateCardDialogFragment.newInstance();
+            dialogFragment.show(getParentFragmentManager(),"CreateCardDialogFragment");
+        });
+
+        return view.getRoot();
+    }
 }
+
