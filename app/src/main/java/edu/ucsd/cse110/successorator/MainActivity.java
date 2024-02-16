f3f7eca6e9d67ec86d32abd088130a0b2e0de302
shruti-create <shruti.bhamidipati@gmail.com>
2024-02-16 13:31:13 -0800

    showing date

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -1,21 +1,35 @@
 package edu.ucsd.cse110.successorator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         var view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
-        //view.placeholderText.setText(R.string.hello_world);
-
+        //view.placeholderText.setText(R.string.hello_world)
         setContentView(view.getRoot());
+        // getting date
+        Calendar c = Calendar.getInstance();
+        String dateString = c.getTime().toString();
+        // showing it on the textview
+        setContentView(R.layout.activity_main);
+        TextView dateView = (TextView)findViewById(R.id.date);
+        dateView.setText(dateString);
     }
 }
