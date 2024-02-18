b490d0ca45b78af56335583df6930c0f9be1f0a7
KaChunLeung <122496133+KaChunLeung@users.noreply.github.com>
2024-02-18 15:51:54 -0800

    ViewModel,MainActivity,SuccessoratorApp Tests

--- app/src/androidTest/java/edu/ucsd/cse110/successorator/MainActivityTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/successorator/MainActivityTest.java
@@ -1,22 +1,17 @@
 package edu.ucsd.cse110.successorator;
 
-import static androidx.test.core.app.ActivityScenario.launch;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.action.ViewActions.click;
+import static org.hamcrest.Matchers.containsString;
 
-import static junit.framework.TestCase.assertEquals;
-
-import android.content.res.Resources;
-import android.widget.TextView;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.core.app.ActivityScenario;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Calendar;
-
-import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,8 +21,21 @@ import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Test
-    /* Function tests if the date is displayed as we made the date display through MainActivity.*/
-    public void TestDisplaysDate() {
-            scenario.moveToState(Lifecycle.State.STARTED);
+    public void dateAndTimeDisplayTest() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            // Assumes date and time are displayed in TextViews with id date and time respectively
+            // Hard to test dynamic content like the current date/time without stubbing or mocking the system clock
+            onView(withId(R.id.date)).check(matches(withText(containsString("Expected Date Format"))));
+            onView(withId(R.id.time)).check(matches(withText(containsString("Expected Time Format"))));
+        }
     }
+    @Test
+    public void createTaskButtonTriggersDialogTest() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            onView(withId(R.id.createTaskButton)).perform(click());
+            onView(withId(R.id.task_text)).check(matches(withText(containsString("Expected Task Text"))));
+        }
+    }
+
 }
+
