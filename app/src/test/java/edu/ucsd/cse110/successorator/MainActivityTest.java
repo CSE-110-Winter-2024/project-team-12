96a9df42f5cdf888088e6bfba0197114712c29bb
shruti-create <shruti.bhamidipati@gmail.com>
2024-02-18 23:43:54 -0800

    main activity tests!

--- app/src/test/java/edu/ucsd/cse110/successorator/MainActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/successorator/MainActivityTest.java
@@ -1,15 +1,34 @@
 package edu.ucsd.cse110.successorator;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.view.MenuItem;
 
-public class MainActivityTest extends TestCase {
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.fakes.RoboMenuItem;
 
+import static org.junit.Assert.assertNotNull;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest=Config.NONE)
+public class MainActivityTest {
+
+    @Test
     public void testOnCreate() {
+        // Create the MainActivity
+        MainActivity mainActivity = new MainActivity();
+
+        assertNotNull(mainActivity);
     }
 
+    @Test
     public void testOnCreateOptionsMenu() {
-    }
-
-    public void testOnOptionsItemSelected() {
+        // Create the MainActivity and tester menuItem
+        MainActivity mainActivity = new MainActivity();
+        MenuItem menuItem = new RoboMenuItem(R.menu.action_bar);
+        // Verify that the menu is not null
+        assertNotNull(menuItem);
     }
 }
