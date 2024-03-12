d621f13d126bd3128b65539090e907f3f59cbb1a
Claire <claire.andolina@gmail.com>
2024-03-11 17:49:25 -0700

    In progress: MainViewModelTest tests

--- app/src/test/java/edu/ucsd/cse110/successorator/MainViewModelTest.java
+++ app/src/test/java/edu/ucsd/cse110/successorator/MainViewModelTest.java
@@ -1,42 +1,68 @@
 package edu.ucsd.cse110.successorator;
 
+import androidx.annotation.Nullable;
+
 import junit.framework.TestCase;
 
 import org.junit.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.ucsd.cse110.successorator.lib.data.InMemoryDataSource;
 import edu.ucsd.cse110.successorator.lib.domain.SimpleTaskRepository;
+import edu.ucsd.cse110.successorator.lib.domain.Tag;
 import edu.ucsd.cse110.successorator.lib.domain.Task;
 import edu.ucsd.cse110.successorator.lib.domain.TaskRepository;
+import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+import edu.ucsd.cse110.successorator.lib.util.Observer;
+import edu.ucsd.cse110.successorator.lib.util.SimpleSubject;
+import edu.ucsd.cse110.successorator.lib.util.Subject;
 
 public class MainViewModelTest extends TestCase {
-/*
+
+    //we will need to update this, along with TaskTest, once we
+    //implement Pending and Recurring date types
     InMemoryDataSource imd = new InMemoryDataSource();
-
-
     SimpleTaskRepository str = new SimpleTaskRepository(imd);
     MainViewModel mvm = new MainViewModel(str);
-    Task testTask1 = new Task(22300,"Clear nostrils",false,3, 1220227200);
-    Task testTask2 = new Task(22301,"Eat my prescription",false,4, 1220227200);
+    Task homeTomorrowTask = new Task(1,"Clean room",false,1, LocalDate.from(LocalDate.now()).plusDays(1), Tag.HOME);
+    Task schoolTodayTask = new Task(2,"Submit reflection",false,2, LocalDate.now(), Tag.SCHOOl);
+    Task errandsTodayTask = new Task(3,"Buy groceries",false,3, LocalDate.from(LocalDate.now()), Tag.ERRANDS);
+     Task workTomorrowTask = new Task(4,"Submit PTO request to boss",false,4, LocalDate.from(LocalDate.from(LocalDate.now().plusDays(1))), Tag.WORK);
 
-
+    /* inserting tasks into InMemoryDataSource out of order ... */
     @Before
     public void setUp(){
-        imd.putTask(new Task(12300,"Take out trash",false,1, 1220227200));
-        imd.putTask(new Task(12301,"Walk the fish",false,2, 1221436800));
+        imd.putTask(homeTomorrowTask);
+        imd.putTask(errandsTodayTask);
+        imd.putTask(workTomorrowTask);
+        imd.putTask(schoolTodayTask);
     }
     @Test
     public void testGetOrderedTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(homeTomorrowTask);
+        tasks.add(schoolTodayTask);
+        tasks.add(errandsTodayTask);
+        tasks.add(workTomorrowTask);
+
+        SimpleSubject<List<Task>> orderedTasks = new SimpleSubject<List<Task>>();
+        orderedTasks.setValue(tasks);
+
+        assertEquals(mvm.getOrderedTasks(), orderedTasks);
     }
 
     public void testGetTaskRepository() {
+        assertEquals(str, mvm.getTaskRepository());
     }
 
+    /*
     public void testSave() {
+
     }
 
     @Test
